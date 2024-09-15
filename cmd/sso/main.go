5953f7d9d37fd57abac6639a7de57979f20a3621
daria_smyr <dasha.smyr@gmail.com>
2024-09-16 00:54:16 +0300

    Added config and log initialisation

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,11 +1,46 @@
 package main
 
-func main() {
-	// TODO: initialize config
+import (
+	"log/slog"
+	"os"
+	"sso/config"
+)
 
-	// TODO: initialize logger
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("sso", "env", cfg.Env)
 
 	// TODO: initialize app
 
 	// TODO: run GRPC server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
