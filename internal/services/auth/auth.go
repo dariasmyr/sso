3a83159dd44ee9b5818959eac35e75f126d6860a
daria_smyr <dasha.smyr@gmail.com>
2024-09-25 22:01:31 +0300

    Added NewToken and Login methods

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -3,8 +3,12 @@ package auth
 import (
 	"context"
 	"errors"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
 	"log/slog"
 	"sso/internal/domain/models"
+	"sso/internal/lib/jwt"
+	"sso/internal/lib/logger/sl"
 	"time"
 )
 
@@ -32,7 +36,7 @@ type AccountProvider interface {
 }
 
 type AppProvider interface {
-	App(ctx context.Context, appId int64) (models.App, error)
+	App(ctx context.Context, appId int) (models.App, error)
 }
 
 type SessionSaver interface {
@@ -64,4 +68,59 @@ func New(
 		sessionProvider: sessionProvider,
 		tokenTTL:        tokenTTL,
 	}
+}
+
+// Login checks if user with given credentials exists in the system and returns access token.
+//
+// If user exists, but password is incorrect, returns error.
+// If user doesn't exist, returns error.
+func (a *Auth) Login(
+	ctx context.Context,
+	email string,
+	password string,
+	appID int,
+) (string, error) {
+	const op = "Auth.Login"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("username", email),
+	)
+
+	log.Info("attempting to login user")
+
+	user, err := a.accountProvider.Account(ctx, email)
+	if err != nil {
+		if errors.Is(err, storage.ErrUserNotFound) {
+			a.log.Warn("user not found", sl.Err(err))
+
+			return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+		}
+
+		a.log.Error("failed to get user", sl.Err(err))
+
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.PassHash, []byte(password)); err != nil {
+		a.log.Info("invalid credentials", sl.Err(err))
+
+		return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+	}
+
+	app, err := a.appProvider.App(ctx, appID)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.Info("user logged in successfully")
+
+	token, err := jwt.NewToken(user, app, a.tokenTTL)
+	if err != nil {
+		a.log.Error("failed to generate token", sl.Err(err))
+
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return token, nil
 }
