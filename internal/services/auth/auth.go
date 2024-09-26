b7eb5c5de8d90efc89d3b3b63e285052deffc846
smyr <dasha.smyr@gmail.com>
2024-09-26 12:00:50 +0300

    Fixed Login and RegisterNewAccount methods with extended business logic

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -2,14 +2,18 @@ package auth
 
 import (
 	"context"
+	"encoding/base64"
 	"errors"
 	"fmt"
-	"golang.org/x/crypto/bcrypt"
 	"log/slog"
 	"sso/internal/domain/models"
 	"sso/internal/lib/jwt"
 	"sso/internal/lib/logger/sl"
 	"time"
+
+	"crypto/rand"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Auth struct {
@@ -20,6 +24,7 @@ type Auth struct {
 	sessionSaver    SessionSaver
 	sessionProvider SessionProvider
 	tokenTTL        time.Duration
+	refreshTokenTTL time.Duration
 }
 
 var (
@@ -40,7 +45,7 @@ type AppProvider interface {
 }
 
 type SessionSaver interface {
-	SaveSession(ctx context.Context, accountId int64, token string, expiresAt time.Time) (sessionID string, err error)
+	SaveSession(ctx context.Context, accountId int64, userAgent string, ipAddress string, expiresAt time.Time) (sessionID string, err error)
 }
 
 type SessionProvider interface {
@@ -58,6 +63,7 @@ func New(
 	sessionSaver SessionSaver,
 	sessionProvider SessionProvider,
 	tokenTTL time.Duration,
+	refreshTokenTTL time.Duration,
 ) *Auth {
 	return &Auth{
 		log:             log,
@@ -67,19 +73,48 @@ func New(
 		sessionSaver:    sessionSaver,
 		sessionProvider: sessionProvider,
 		tokenTTL:        tokenTTL,
+		refreshTokenTTL: refreshTokenTTL,
 	}
 }
 
-// Login checks if user with given credentials exists in the system and returns access token.
+// RegisterNewAccount registers a new account in the system, creates a session, and returns account ID.
+func (a *Auth) RegisterNewAccount(ctx context.Context, email string, pass string, userAgent string, ipAddress string) (int64, error) {
+	const op = "Auth.RegisterNewAccount"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("email", email),
+	)
+
+	log.Info("registering account")
+
+	passHash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	if err != nil {
+		log.Error("failed to generate password hash", sl.Err(err))
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := a.accountSaver.SaveAccount(ctx, email, passHash)
+	if err != nil {
+		log.Error("failed to save account", sl.Err(err))
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
+}
+
+// Login checks if account with given credentials exists in the system and returns access + refresh token.
 //
-// If user exists, but password is incorrect, returns error.
-// If user doesn't exist, returns error.
+// If account exists, but password is incorrect, returns error.
+// If account doesn't exist, returns error.
 func (a *Auth) Login(
 	ctx context.Context,
 	email string,
 	password string,
+	userAgent string,
+	ipAddress string,
 	appID int,
-) (string, error) {
+) (string, string, error) {
 	const op = "Auth.Login"
 
 	log := a.log.With(
@@ -93,24 +128,21 @@ func (a *Auth) Login(
 	if err != nil {
 		if errors.Is(err, storage.ErrUserNotFound) {
 			a.log.Warn("user not found", sl.Err(err))
-
-			return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+			return "", "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
 		}
 
 		a.log.Error("failed to get user", sl.Err(err))
-
-		return "", fmt.Errorf("%s: %w", op, err)
+		return "", "", fmt.Errorf("%s: %w", op, err)
 	}
 
 	if err := bcrypt.CompareHashAndPassword(user.PassHash, []byte(password)); err != nil {
 		a.log.Info("invalid credentials", sl.Err(err))
-
-		return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+		return "", "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
 	}
 
 	app, err := a.appProvider.App(ctx, appID)
 	if err != nil {
-		return "", fmt.Errorf("%s: %w", op, err)
+		return "", "", fmt.Errorf("%s: %w", op, err)
 	}
 
 	log.Info("user logged in successfully")
@@ -118,9 +150,34 @@ func (a *Auth) Login(
 	token, err := jwt.NewToken(user, app, a.tokenTTL)
 	if err != nil {
 		a.log.Error("failed to generate token", sl.Err(err))
-
-		return "", fmt.Errorf("%s: %w", op, err)
+		return "", "", fmt.Errorf("%s: %w", op, err)
 	}
 
-	return token, nil
+	refreshToken, err := generateRefreshToken()
+	if err != nil {
+		log.Error("failed to generate refresh token", sl.Err(err))
+		return "", "", fmt.Errorf("%s: %w", op, err)
+	}
+	expiresAt := time.Now().Add(a.refreshTokenTTL)
+
+	sessionID, err := a.sessionSaver.SaveSession(ctx, user.ID, userAgent, ipAddress, expiresAt)
+	if err != nil {
+		a.log.Error("failed to save session", sl.Err(err))
+		return "", "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.Info("session created", slog.String("session_id", sessionID))
+
+	return token, refreshToken, nil
+}
+
+func generateRefreshToken() (string, error) {
+	const tokenSize = 32
+	token := make([]byte, tokenSize)
+
+	if _, err := rand.Read(token); err != nil {
+		return "", err
+	}
+
+	return base64.URLEncoding.EncodeToString(token), nil
 }
