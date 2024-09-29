0f6c44594efe7ced23cb7e82d89c0a9c536cb1a9
daria_smyr <dasha.smyr@gmail.com>
2024-09-29 23:58:26 +0300

    Added methods for work with account and sessions in db

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -8,6 +8,7 @@ import (
 	"github.com/mattn/go-sqlite3"
 	"sso/internal/domain/models"
 	"sso/internal/storage"
+	"time"
 )
 
 type Storage struct {
@@ -100,4 +101,198 @@ func (s *Storage) App(ctx context.Context, id int) (models.App, error) {
 	}
 
 	return app, nil
+}
+
+func (s *Storage) AccountByEmail(ctx context.Context, email string) (models.Account, error) {
+	const op = "storage.sqlite.AccountByEmail"
+
+	stmt, err := s.db.Prepare("SELECT id, email, pass_hash, role, status, app_id FROM accounts WHERE email = ?")
+	if err != nil {
+		return models.Account{}, fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	var account models.Account
+	err = stmt.QueryRowContext(ctx, email).Scan(&account.ID, &account.Email, &account.PassHash, &account.Role, &account.Status, &account.AppId)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.Account{}, fmt.Errorf("%s: %w", op, storage.ErrAccountNotFound)
+		}
+		return models.Account{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return account, nil
+}
+
+func (s *Storage) AccountById(ctx context.Context, accountId int64) (models.Account, error) {
+	const op = "storage.sqlite.AccountById"
+
+	stmt, err := s.db.Prepare("SELECT id, email, pass_hash, role, status, app_id FROM accounts WHERE id = ?")
+	if err != nil {
+		return models.Account{}, fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	var account models.Account
+	err = stmt.QueryRowContext(ctx, accountId).Scan(&account.ID, &account.Email, &account.PassHash, &account.Role, &account.Status, &account.AppId)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.Account{}, fmt.Errorf("%s: %w", op, storage.ErrAccountNotFound)
+		}
+		return models.Account{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return account, nil
+}
+
+func (s *Storage) UpdatePassword(ctx context.Context, accountId int64, newPassHash []byte) error {
+	const op = "storage.sqlite.UpdatePassword"
+
+	stmt, err := s.db.Prepare("UPDATE accounts SET pass_hash = ? WHERE id = ?")
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, newPassHash, accountId)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
+func (s *Storage) UpdateStatus(ctx context.Context, accountId int64, status models.AccountStatus) error {
+	const op = "storage.sqlite.UpdateStatus"
+
+	stmt, err := s.db.Prepare("UPDATE accounts SET status = ? WHERE id = ?")
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, status, accountId)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
+func (s *Storage) SaveSession(ctx context.Context, accountId int64, userAgent, ipAddress, token, refreshToken string, expiresAt time.Time) (string, error) {
+	const op = "storage.sqlite.SaveSession"
+
+	stmt, err := s.db.Prepare(`
+		INSERT INTO sessions (account_id, token, refresh_token, user_agent, ip_address, expires_at, refresh_expires_at) 
+		VALUES (?, ?, ?, ?, ?, ?, ?)
+	`)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	refreshExpiresAt := expiresAt.Add(7 * 24 * time.Hour) // Время жизни refresh-токена
+
+	_, err = stmt.ExecContext(ctx, accountId, token, refreshToken, userAgent, ipAddress, expiresAt, refreshExpiresAt)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return token, nil
+}
+
+func (s *Storage) Sessions(ctx context.Context, accountId int64) ([]models.Session, error) {
+	const op = "storage.sqlite.Sessions"
+
+	stmt, err := s.db.Prepare(`
+		SELECT id, account_id, token, refresh_token, user_agent, ip_address, expires_at, refresh_expires_at, revoked 
+		FROM sessions WHERE account_id = ? AND revoked = 0
+	`)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.QueryContext(ctx, accountId)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	defer rows.Close()
+
+	var sessions []models.Session
+	for rows.Next() {
+		var session models.Session
+		err := rows.Scan(&session.ID, &session.AccountID, &session.Token, &session.RefreshToken, &session.UserAgent, &session.IPAddress, &session.ExpiresAt, &session.RefreshExpiresAt, &session.Revoked)
+		if err != nil {
+			return nil, fmt.Errorf("%s: %w", op, err)
+		}
+		sessions = append(sessions, session)
+	}
+
+	return sessions, nil
+}
+
+func (s *Storage) Session(ctx context.Context, token string) (models.Session, error) {
+	const op = "storage.sqlite.Session"
+
+	stmt, err := s.db.Prepare(`
+		SELECT id, account_id, token, refresh_token, user_agent, ip_address, expires_at, refresh_expires_at, revoked 
+		FROM sessions WHERE token = ?
+	`)
+	if err != nil {
+		return models.Session{}, fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	var session models.Session
+	err = stmt.QueryRowContext(ctx, token).Scan(&session.ID, &session.AccountID, &session.Token, &session.RefreshToken, &session.UserAgent, &session.IPAddress, &session.ExpiresAt, &session.RefreshExpiresAt, &session.Revoked)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.Session{}, fmt.Errorf("%s: %w", op, storage.ErrSessionNotFound)
+		}
+		return models.Session{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return session, nil
+}
+
+func (s *Storage) SessionByRefreshToken(ctx context.Context, refreshToken string) (models.Session, error) {
+	const op = "storage.sqlite.SessionByRefreshToken"
+
+	stmt, err := s.db.Prepare(`
+		SELECT id, account_id, token, refresh_token, user_agent, ip_address, expires_at, refresh_expires_at, revoked 
+		FROM sessions WHERE refresh_token = ?
+	`)
+	if err != nil {
+		return models.Session{}, fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	var session models.Session
+	err = stmt.QueryRowContext(ctx, refreshToken).Scan(&session.ID, &session.AccountID, &session.Token, &session.RefreshToken, &session.UserAgent, &session.IPAddress, &session.ExpiresAt, &session.RefreshExpiresAt, &session.Revoked)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.Session{}, fmt.Errorf("%s: %w", op, storage.ErrSessionNotFound)
+		}
+		return models.Session{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return session, nil
+}
+
+func (s *Storage) RevokeSession(ctx context.Context, token string) error {
+	const op = "storage.sqlite.RevokeSession"
+
+	stmt, err := s.db.Prepare("UPDATE sessions SET revoked = 1 WHERE token = ?")
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, token)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
 }
