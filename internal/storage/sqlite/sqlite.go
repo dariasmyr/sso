36d141e2dc7771de394b2d92c80ab2bba40ccf4b
daria_smyr <dasha.smyr@gmail.com>
2024-09-29 23:47:12 +0300

    Added sqlite.go

---
+++ internal/storage/sqlite/sqlite.go
@@ -0,0 +1,80 @@
+package sqlite
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.com/mattn/go-sqlite3"
+	"sso/internal/domain/models"
+	"sso/internal/storage"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(storagePath string) (*Storage, error) {
+	const op = "storage.sqlite.New"
+
+	db, err := sql.Open("sqlite3", storagePath)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveAccount(ctx context.Context, email string, passHash []byte, status int, appID *int64, role int) (int64, error) {
+	const op = "storage.sqlite.SaveAccount"
+
+	stmt, err := s.db.Prepare(`
+		INSERT INTO accounts (email, pass_hash, status, app_id, role) 
+		VALUES (?, ?, ?, ?, ?)
+	`)
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	defer stmt.Close()
+
+	res, err := stmt.ExecContext(ctx, email, passHash, status, appID, role)
+	if err != nil {
+		var sqliteErr sqlite3.Error
+
+		if errors.As(err, &sqliteErr) && errors.Is(sqliteErr, sqlite3.ErrConstraintUnique) {
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrAccountExists)
+		}
+
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
+}
+
+func (s *Storage) Account(ctx context.Context, email string) (models.Account, error) {
+	const op = "storage.sqlite.Account"
+
+	stmt, err := s.db.Prepare("SELECT id, email, pass_hash FROM users WHERE email = ?")
+	if err != nil {
+		return models.Account{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	row := stmt.QueryRowContext(ctx, email)
+
+	var account models.Account
+	err = row.Scan(&account.ID, &account.Email, &account.PassHash)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.Account{}, fmt.Errorf("%s: %w", op, storage.ErrAccountNotFound)
+		}
+
+		return models.Account{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return account, nil
+}
