e65f3bb530f2070067a2d4e545a883978673518f
daria_smyr <dasha.smyr@gmail.com>
2024-10-03 00:49:15 +0300

    Added interceptors and main app

---
+++ internal/app/grpc/app.go
@@ -0,0 +1,100 @@
+package grpcapp
+
+import (
+	"context"
+	"fmt"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/recovery"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"log/slog"
+	"net"
+	authgrpc "sso/internal/grpc/auth"
+
+	"google.golang.org/grpc"
+)
+
+type App struct {
+	log        *slog.Logger
+	gRPCServer *grpc.Server
+	port       int
+}
+
+func maskSensitiveFields(fields []any) []any {
+	for i := 0; i < len(fields)-1; i += 2 {
+		key, ok := fields[i].(string)
+		if !ok {
+			continue
+		}
+		if key == "password" || key == "pwd" || key == "pass" {
+			if pwd, ok := fields[i+1].(string); ok {
+				if len(pwd) > 4 {
+					fields[i+1] = pwd[:1] + "****" + pwd[len(pwd)-1:]
+				} else {
+					fields[i+1] = "****"
+				}
+			}
+		}
+	}
+	return fields
+}
+
+func InterceptorLogger(l *slog.Logger) logging.Logger {
+	return logging.LoggerFunc(func(ctx context.Context, lvl logging.Level, msg string, fields ...any) {
+		maskedFields := maskSensitiveFields(fields)
+		l.Log(ctx, slog.Level(lvl), msg, maskedFields...)
+	})
+}
+
+func New(log *slog.Logger, authService authgrpc.Auth, port int) *App {
+	loggingOpts := []logging.Option{
+		logging.WithLogOnEvents(
+			logging.PayloadReceived, logging.PayloadSent,
+		),
+	}
+
+	recoveryOpts := []recovery.Option{
+		recovery.WithRecoveryHandler(func(p interface{}) (err error) {
+			log.Error("Recovered from panic", slog.Any("panic", p))
+
+			return status.Errorf(codes.Internal, "Panic occurred: %v", p)
+			// return status.Errorf(codes.Internal, "internal error") in case we want the error to be hidden from client
+		}),
+	}
+
+	gRPCServer := grpc.NewServer(grpc.ChainUnaryInterceptor(
+		recovery.UnaryServerInterceptor(recoveryOpts...),
+		logging.UnaryServerInterceptor(InterceptorLogger(log), loggingOpts...),
+	))
+
+	authgrpc.Register(gRPCServer, authService)
+
+	return &App{
+		log:        log,
+		gRPCServer: gRPCServer,
+		port:       port,
+	}
+}
+
+func (a *App) MustRun() {
+	if err := a.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func (a *App) Run() error {
+	const op = "grpcapp.Run"
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	a.log.Info("grpc server started", slog.String("addr", l.Addr().String()))
+
+	if err := a.gRPCServer.Serve(l); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
