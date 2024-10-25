1d2a9e5f4726a4dcd11d5819a0a1768a41dd7a43
smyr <dasha.smyr@gmail.com>
2024-10-25 10:51:57 +0300

    Implemented customized claims in JWT management

--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 require (
 	github.com/BurntSushi/toml v1.4.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
--- internal/lib/jwt/jwt.go
+++ internal/lib/jwt/jwt.go
@@ -1,23 +1,40 @@
 package jwt
 
 import (
+	"errors"
 	"fmt"
 	"sso/internal/domain/models"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
-// NewToken creates new JWT token for given user and app.
-func NewToken(user models.Account, app models.App, duration time.Duration) (string, error) {
-	token := jwt.New(jwt.SigningMethodHS256)
+type CustomClaims struct {
+	UserID int64              `json:"uid"`
+	Email  string             `json:"email"`
+	Role   models.AccountRole `json:"role"`
+	AppID  int64              `json:"app_id"`
+	jwt.RegisteredClaims
+}
 
-	claims := token.Claims.(jwt.MapClaims)
-	claims["uid"] = user.ID
-	claims["email"] = user.Email
-	claims["exp"] = time.Now().Add(duration).Unix()
-	claims["app_id"] = app.ID
-	claims["jti"] = generateJTI()
+func NewToken(user models.Account, app models.App, duration time.Duration) (string, error) {
+	if user.ID == 0 || app.ID == 0 || app.Secret == "" {
+		return "", errors.New("недостаточно данных для создания токена")
+	}
+
+	claims := CustomClaims{
+		UserID: user.ID,
+		Email:  user.Email,
+		Role:   user.Role,
+		AppID:  app.ID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(duration)),
+			ID:        generateJTI(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	tokenString, err := token.SignedString([]byte(app.Secret))
 	if err != nil {
@@ -27,6 +44,26 @@ func NewToken(user models.Account, app models.App, duration time.Duration) (stri
 	return tokenString, nil
 }
 
+func ParseToken(tokenString string, secret string) (*CustomClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
+
+	if err != nil || !token.Valid {
+		return nil, errors.New("неверный токен")
+	}
+
+	claims, ok := token.Claims.(*CustomClaims)
+	if !ok {
+		return nil, errors.New("не удалось получить утверждения из токена")
+	}
+
+	return claims, nil
+}
+
 func generateJTI() string {
-	return fmt.Sprintf("%d", time.Now().UnixNano())
+	return uuid.New().String()
 }
