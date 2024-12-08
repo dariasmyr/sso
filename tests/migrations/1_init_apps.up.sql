79d152f56f75432b8d16666745131d32f211de31
daria_smyr <dasha.smyr@gmail.com>
2024-12-08 22:02:20 +0300

    Fixed test init migration

--- tests/migrations/1_init_apps.up.sql
+++ tests/migrations/1_init_apps.up.sql
@@ -1,3 +1,46 @@
+CREATE TABLE IF NOT EXISTS accounts
+(
+    id           INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    email        TEXT NOT NULL UNIQUE,
+    pass_hash    BYTEA NOT NULL,
+    status       INTEGER NOT NULL, -- AccountStatus (0 - ACTIVE, 1 - INACTIVE, 2 - DELETED)
+    app_id       BIGINT NOT NULL REFERENCES apps(id),
+    role        INTEGER NOT NULL, -- AccountRoles (0 - USER, 1 - ADMIN)
+    CONSTRAINT valid_status CHECK (status IN (0, 1, 2))
+    );
+
+CREATE INDEX IF NOT EXISTS idx_email ON accounts (email);
+
+CREATE TABLE IF NOT EXISTS apps
+(
+    id        INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    name      TEXT NOT NULL UNIQUE,
+    secret    TEXT NOT NULL UNIQUE,
+    redirect_url TEXT
+);
+
+
+CREATE TABLE IF NOT EXISTS sessions
+(
+    id                INTEGER PRIMARY KEY AUTOINCREMENT,
+    account_id        BIGINT NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
+    token             TEXT NOT NULL UNIQUE,
+    refresh_token     TEXT NOT NULL,
+    user_agent        TEXT,
+    ip_address        TEXT,
+    expires_at        TIMESTAMP NOT NULL,
+    refresh_expires_at TIMESTAMP NOT NULL,
+    created_at        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    revoked           BOOLEAN NOT NULL DEFAULT FALSE
+    );
+
+CREATE INDEX IF NOT EXISTS idx_account_id ON sessions (account_id);
+
 INSERT INTO apps (id, name, secret, redirect_url)
 VALUES (1, 'test', 'test-secret', 'http://localhost:3000')
     ON CONFLICT DO NOTHING;
